e1219d58c94c24ba96f9cfc950dad7d733853976
Jeroen Baeten <jbaeten@ctg.com>
2024-07-22 16:22:40 +0200

    Script to kill port after tests are finished

--- JenkinsDemo_Selenium/src/test/java/StepDefinitions/CalculatorStepdefinitions.java
+++ JenkinsDemo_Selenium/src/test/java/StepDefinitions/CalculatorStepdefinitions.java
@@ -6,7 +6,6 @@ import Helpers.TestscriptHelper;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.And;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;

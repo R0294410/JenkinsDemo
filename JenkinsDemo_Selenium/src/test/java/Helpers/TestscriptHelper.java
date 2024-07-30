4d24d9a5c3a1daeb16f7735405af1b8600217fc3
Jeroen Baeten <jbaeten@ctg.com>
2024-07-30 08:04:24 +0200

    Revert "Script to kill port after tests are finished"
    This reverts commit e1219d58c94c24ba96f9cfc950dad7d733853976.

--- JenkinsDemo_Selenium/src/test/java/Helpers/TestscriptHelper.java
+++ JenkinsDemo_Selenium/src/test/java/Helpers/TestscriptHelper.java
@@ -1,24 +1,18 @@
 package Helpers;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public class TestscriptHelper {
     public static boolean applicationIsLaunched = false;
 
     @BeforeClass
-    public static void SetupApplicationLocally() {
+    public static void SetupApplicationLocally(){
         DotnetLauncherHelper.StartApplication();
         try {
             Thread.sleep(10000);
             applicationIsLaunched = true;
-        } catch (InterruptedException ex) {
+        }catch (InterruptedException ex){
             ex.printStackTrace();
         }
-    }
-
-    @AfterClass
-    public static void ClearLocalApplication() {
-        PortKiller.StopApplication();
     }
 }
